9a7b07d646e0983f7a17b8c0bc30e1b9c748e369
rhaidet <radu.haidet@3pillarglobal.com>
2021-01-11 20:52:43 +0200

    Automation practice: - created project - added gitignore/readme/req - created base class and changed path for driver

---
+++ SampleProjects/AutomationPractice/Tests/TestSetup.py
@@ -0,0 +1,20 @@
+from selenium import webdriver
+import unittest
+import sys
+import os
+
+
+sys.path.append(os.path.join(os.path.dirname(__file__), "...", "..."))
+
+class TestSetup(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        cls.driver = webdriver.Chrome(executable_path=sys.path[1]+"/drivers/chromedriver")
+        cls.driver.maximize_window()
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.driver.close()
+        cls.driver.quit()
+        print("Test completed")
