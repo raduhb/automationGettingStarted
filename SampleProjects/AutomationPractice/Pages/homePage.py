7277618025700ba1b5566d6174eaf5a3e620e422
rhaidet <radu.haidet@3pillarglobal.com>
2021-01-12 14:54:39 +0200

    AutomationPractice - added test for enter key press

--- SampleProjects/AutomationPractice/Pages/homePage.py
+++ SampleProjects/AutomationPractice/Pages/homePage.py
@@ -7,6 +7,9 @@ from SampleProjects.AutomationPractice.Locators.locators import Locators
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.keys import Keys
+
+
 
 class HomePage(Actions):
 
@@ -16,7 +19,7 @@ class HomePage(Actions):
         self.search_textbox_id = Locators.search_textbox_id
         self.search_button_name = Locators.search_button_name
 
-    def enter_search_term(self, search_term):
+    def enter_search_term_and_click_search_button(self, search_term):
         try:
             # WebDriverWait(self.driver, 2).until(
             #     EC.visibility_of_element_located((By.ID, self.search_textbox_id))
@@ -30,6 +33,22 @@ class HomePage(Actions):
                 EC.visibility_of_element_located((By.NAME, self.search_button_name))
             )
             self.driver.find_element(By.NAME, self.search_button_name).click()
+        except Exception as e:
+            # print(f"Test failed due to", {e})
+            traceback.print_exc()
+            raise e
+
+    def enter_search_term_and_press_enter_key(self, search_term):
+        try:
+            # WebDriverWait(self.driver, 2).until(
+            #     EC.visibility_of_element_located((By.ID, self.search_textbox_id))
+            # )
+            # self.driver.find_element(By.ID, self.search_textbox_id).clear()
+            # self.driver.find_element(By.ID, self.search_textbox_id).send_keys(search_term)
+
+            Actions.find_by_id_and_enter_term(self, self.search_textbox_id, search_term)
+            self.driver.find_element(By.ID, self.search_textbox_id).send_keys(Keys.RETURN)
+
         except Exception as e:
             # print(f"Test failed due to", {e})
             traceback.print_exc()
--- SampleProjects/AutomationPractice/Tests/search.py
+++ SampleProjects/AutomationPractice/Tests/search.py
@@ -2,22 +2,35 @@ import time
 import unittest
 import sys
 import os
+# sys.path.append(os.path.join(os.path.dirname(__file__), "...", "..."))
+import export
 
+# export PYTHONPATH="${PYTHONPATH}:~/.PycharmProjects/pythonProject/SampleProjects/AutomationPractice/"
+sys.path.append("~/.PycharmProjects/pythonProject/SampleProjects/AutomationPractice/")
 from SampleProjects.AutomationPractice.Pages.homePage import HomePage
 from SampleProjects.AutomationPractice.Tests.TestSetup import TestSetup
 
-sys.path.append(os.path.join(os.path.dirname(__file__), "...", "..."))
 
 
 class SearchTest(TestSetup):
 
-    def test_perform_search(self):
+    def test_perform_search_by_clicking_search_button(self):
         driver = self.driver
 
         driver.get("http://automationpractice.com")
 
         home = HomePage(driver)
-        home.enter_search_term('test')
+        home.enter_search_term_and_click_search_button('test')
+
+        time.sleep(5)
+
+    def test_perform_search_by_pressing_enter_key(self):
+        driver = self.driver
+
+        driver.get("http://automationpractice.com")
+
+        home = HomePage(driver)
+        home.enter_search_term_and_press_enter_key('test')
 
         time.sleep(5)
 
